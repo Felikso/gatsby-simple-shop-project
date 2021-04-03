2d3961ad2f3a9ff3318e580c400fc4db9e0efa0f
Felikso <aleksandrasniegucka@wp.pl>
2021-04-03 16:09:18 +0200

    added optimalization part

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,8 +55,19 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-persist-cache`,
+      options: {
+        enabled: !process.env.DISABLE_PERSISTENT_CACHE,
+        persistentDir: ".persistent-cache",
+        pluginNames: [`your-plugin-names-here`],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-webpack-bundle-analyser-v2",
+      options: {
+        devMode: true,
+      },
+    },
   ],
 }
